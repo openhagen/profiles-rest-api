94e0b258cd9b31c435b268cb080e3909d1af414a
openhagen <p.v.sopnev@outlook.com>
2024-01-22 11:23:25 +0100

    Made the script executable

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'l7pq#3=eer1udf2h9@$nd0f($63sg0h%bw2))l#md!3+brj3q$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT - 'static/'
